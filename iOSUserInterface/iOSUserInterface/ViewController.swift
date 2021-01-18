1beeafa49b36a0b0312d5dcc1779f73158e8d3b3
Apodolsky <sacha55@yandex.ru>
2021-01-18 23:08:05 +0300

    Homework1

---
+++ iOSUserInterface/iOSUserInterface/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSUserInterface
+//
+//  Created by Apodolsky on 18.01.2021.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField?
+    @IBOutlet weak var passwordTextField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
